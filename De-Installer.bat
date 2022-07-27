ac7ab541ea2640d3af1ec48f59a448691181a756
bijenmanlol <85669046+bijenmanlol@users.noreply.github.com>
2022-07-27 19:29:39 +0200

    Update De-Installer.bat

--- De-Installer.bat
+++ De-Installer.bat
@@ -1,2 +1,6 @@
 cd %USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 del OofReturnerRun.bat
+cd %USERPROFILE%\AppData\Local\Roblox\Versions
+FOR /F "delims=" %%i IN ('dir /b /ad-h /t:w /od') DO SET a=%%i
+cd %a%\content\sounds
+curl "https://raw.githubusercontent.com/bijenmanlol/return-oof/main/badOuch.ogg" --output 
