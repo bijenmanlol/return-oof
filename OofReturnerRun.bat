0f1886c76f9c7bdc70c01e115db21abc171df388
bijenmanlol <85669046+bijenmanlol@users.noreply.github.com>
2022-07-27 19:00:08 +0200

    Add files via upload

---
+++ OofReturnerRun.bat
@@ -0,0 +1,6 @@
+cd %USERPROFILE%\AppData\Local\Roblox\Versions
+FOR /F "delims=" %%i IN ('dir /b /ad-h /t:w /od') DO SET a=%%i
+cd %a%\content\sounds
+curl "https://raw.githubusercontent.com/bijenmanlol/return-oof/main/ouch.ogg" --output ouch.ogg
+
+
