30ec1cf97ab1e43ce6ca6058dccc9275330bb7af
bijenmanlol <85669046+bijenmanlol@users.noreply.github.com>
2022-07-28 23:12:09 +0200

    Update Installer.bat

--- Installer.bat
+++ Installer.bat
@@ -1,4 +1,8 @@
-cd %USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+cd %USERPROFILE%\AppData\Roaming
+mkdir return-oof
+cd return-oof
 curl "https://raw.githubusercontent.com/bijenmanlol/return-oof/main/OofReturnerRun.bat" --output OofReturnerRun.bat
-call OofReturnerRun.bat
+cd %USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+curl "https://raw.githubusercontent.com/bijenmanlol/return-oof/blob/main/SilentStartOof.bat" --output SilentStartOof.bat
+call SilentStartOof.bat
 msg "%username%" Return-oof is installed
