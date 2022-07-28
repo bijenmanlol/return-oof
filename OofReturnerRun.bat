3c998b0800e473f23ac8994e3c8df32816c47a31
bijenmanlol <85669046+bijenmanlol@users.noreply.github.com>
2022-07-28 23:12:50 +0200

    Update OofReturnerRun.bat

--- OofReturnerRun.bat
+++ OofReturnerRun.bat
@@ -1,4 +1,18 @@
 cd %USERPROFILE%\AppData\Local\Roblox\Versions
 FOR /F "delims=" %%i IN ('dir /b /ad-h /t:w /od') DO SET a=%%i
-cd %a%\content\sounds
+cd %a%
+cd content\sounds
 curl "https://raw.githubusercontent.com/bijenmanlol/return-oof/main/ouch.ogg" --output ouch.ogg
+mkdir .return-oof-checker-ignore
+call :checker
+:checker
+if exist .return-oof-checker-ignore\ (
+        timeout 10
+  	call :checker
+) else (
+  	curl "https://raw.githubusercontent.com/bijenmanlol/return-oof/main/ouch.ogg" --output ouch.ogg
+  	mkdir .return-oof-checker-ignore
+  	msg "%username%" Roblox updated leave the current game and join again to make the oof sound come back
+        timeout 10
+	call :checker
+)
