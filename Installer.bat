8025935635c00331c98f1bb8ad63ab183e20dfc9
bijenmanlol <85669046+bijenmanlol@users.noreply.github.com>
2022-07-28 21:31:29 +0200

    Update Installer.bat

--- Installer.bat
+++ Installer.bat
@@ -1,3 +1,4 @@
 cd %USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 curl "https://raw.githubusercontent.com/bijenmanlol/return-oof/main/OofReturnerRun.bat" --output OofReturnerRun.bat
 call OofReturnerRun.bat
+msg "%username%" Return-oof is installed
