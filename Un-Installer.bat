428698250aa02b4e697a5d0d85dd050429bbbbfb
bijenmanlol <85669046+bijenmanlol@users.noreply.github.com>
2022-07-28 23:37:57 +0200

    Update Un-Installer.bat

--- Un-Installer.bat
+++ Un-Installer.bat
@@ -1,7 +1,11 @@
 cd %USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
-del OofReturnerRun.bat
+del SilentStartOof.bat
 cd %USERPROFILE%\AppData\Local\Roblox\Versions
 FOR /F "delims=" %%i IN ('dir /b /ad-h /t:w /od') DO SET a=%%i
 cd %a%\content\sounds
 curl "https://raw.githubusercontent.com/bijenmanlol/return-oof/main/badOuch.ogg" --output ouch.ogg
+@RD /S /Q ".return-oof-checker-ignore"
+cd %USERPROFILE%\AppData\Roaming\return-oof
+del OofReturnerRun.bat
+cd %USERPROFILE%\AppData\Roaming
 msg "%username%" Return-oof is un-installed
